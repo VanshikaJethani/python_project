91a353d912154ba8a65edbd65ac491ed00eb3205
vanshika <vanshikajethani37@gmail.com>
2021-10-29 10:14:29 +0530

    first commit

---
+++ series.py
@@ -0,0 +1,11 @@
+def sum(n): 
+    i = 1
+    s = 0.0
+    for i in range(1, n+1): 
+        s = s + 1/i; 
+    return s; 
+  
+
+n= int(input("Enter value of N :- "))
+
+print("Sum is", round(sum(n), 6)) 
